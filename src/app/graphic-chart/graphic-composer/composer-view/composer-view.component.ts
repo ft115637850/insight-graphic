9695c6d33feef4b5c497704976b787fe8598b6cf
Newton Zou <gentleman000000@hotmail.com>
2019-06-17 17:42:35 +0800

    Add color palettes list

--- src/app/graphic-chart/graphic-composer/composer-view/composer-view.component.ts
+++ src/app/graphic-chart/graphic-composer/composer-view/composer-view.component.ts
@@ -7,12 +7,13 @@ import { SymbolInfo } from '../../interfaces/symbol-info.data';
 import { TagInfo } from '../../interfaces/tag-info.data';
 import { v4 as uuid } from 'uuid';
 
-/*
+
 interface Resolution {
-  value: number;
+  x: number;
+  y: number;
   viewValue: string;
 }
-*/
+
 @Component({
   selector: 'app-composer-view',
   templateUrl: './composer-view.component.html',
@@ -31,61 +32,61 @@ export class ComposerViewComponent implements OnInit {
   tagList: TagInfo[];
   symbolList: SymbolInfo[] = [];
   focusedSymbols: SymbolInfo[] = [];
+  resolutionsLst: Resolution[] = [];
   @ViewChild('contentHolder')
   contentHolder: ElementRef;
   @ViewChild('mainCanvas')
   mainCanvas: ElementRef;
   private bgImageWidth: number;
   private bgImageHeight: number;
-  /*
-    resolutions: Resolution[] = [
-      { value: 4 / 3, viewValue: '640x480' },
-      { value: 4 / 3, viewValue: '800x600' },
-      { value: 4 / 3, viewValue: '320x240' },
-      { value: 4 / 3, viewValue: '160x120' },
-      { value: 3 / 2, viewValue: '240x160' },
-      { value: 15 / 9, viewValue: '400x240' },
-      { value: 3 / 2, viewValue: '480x320' },
-      { value: 16 / 10, viewValue: '768x480' },
-      { value: 16 / 9, viewValue: '854x480' },
-      { value: 3 / 2, viewValue: '960x640' },
-      { value: 16 / 9, viewValue: '1024x576/600' },
-      { value: 4 / 3, viewValue: '1024x768' },
-      { value: 16 / 9, viewValue: '1366x768' },
-      { value: 4 / 3, viewValue: '1152x864' },
-      { value: 8 / 5, viewValue: '1440x900' },
-      { value: 5 / 4, viewValue: '1280x1024' },
-      { value: 4 / 3, viewValue: '1400x1050' },
-      { value: 16 / 10, viewValue: '1680x1050' },
-      { value: 4 / 3, viewValue: '1600x1200' },
-      { value: 16 / 10, viewValue: '1920x1200' },
-      { value: 16 / 9, viewValue: '2048x1152' },
-      { value: 4 / 3, viewValue: '2048x1536' },
-      { value: 16 / 10, viewValue: '2560x1600' },
-      { value: 5 / 4, viewValue: '2560x2048' },
-      { value: 25 / 16, viewValue: '3200x2048' },
-      { value: 4 / 3, viewValue: '3200x2400' },
-      { value: 16 / 10, viewValue: '3840x2400' },
-      { value: 4 / 3, viewValue: '4096x3072' },
-      { value: 16 / 10, viewValue: '5120x3200' },
-      { value: 5 / 4, viewValue: '5120x4096' },
-      { value: 25 / 16, viewValue: '6400x4096' },
-      { value: 4 / 3, viewValue: '6400x4800' },
-      { value: 8 / 5, viewValue: '7680x4800' },
-      { value: 16 / 9, viewValue: '1280x720' },
-      { value: 16 / 9, viewValue: '1920x1080' },
-      { value: 16 / 9, viewValue: '640x360' },
-      { value: 16 / 9, viewValue: '960x540' },
-      { value: 16 / 9, viewValue: '2560x1440' },
-      { value: 16 / 9, viewValue: '2560x1440' },
-      { value: 21 / 9, viewValue: '3440x1440' },
-      { value: 16 / 9, viewValue: '3200x1800' },
-      { value: 16 / 9, viewValue: '3200x1800' },
-      { value: 16 / 9, viewValue: '3840x2160' },
-      { value: 16 / 9, viewValue: '5120x2880' },
-      { value: 16 / 9, viewValue: '7680x4320' },
-    ];
-  */
+  private resolutions: Resolution[] = [
+    { x: 4, y: 3, viewValue: '640x480' },
+    { x: 4, y: 3, viewValue: '800x600' },
+    { x: 4, y: 3, viewValue: '320x240' },
+    { x: 4, y: 3, viewValue: '160x120' },
+    { x: 3, y: 2, viewValue: '240x160' },
+    { x: 15, y: 9, viewValue: '400x240' },
+    { x: 3, y: 2, viewValue: '480x320' },
+    { x: 16, y: 10, viewValue: '768x480' },
+    { x: 16, y: 9, viewValue: '854x480' },
+    { x: 3, y: 2, viewValue: '960x640' },
+    { x: 16, y: 9, viewValue: '1024x576/600' },
+    { x: 4, y: 3, viewValue: '1024x768' },
+    { x: 16, y: 9, viewValue: '1366x768' },
+    { x: 4, y: 3, viewValue: '1152x864' },
+    { x: 8, y: 5, viewValue: '1440x900' },
+    { x: 5, y: 4, viewValue: '1280x1024' },
+    { x: 4, y: 3, viewValue: '1400x1050' },
+    { x: 16, y: 10, viewValue: '1680x1050' },
+    { x: 4, y: 3, viewValue: '1600x1200' },
+    { x: 16, y: 10, viewValue: '1920x1200' },
+    { x: 16, y: 9, viewValue: '2048x1152' },
+    { x: 4, y: 3, viewValue: '2048x1536' },
+    { x: 16, y: 10, viewValue: '2560x1600' },
+    { x: 5, y: 4, viewValue: '2560x2048' },
+    { x: 25, y: 16, viewValue: '3200x2048' },
+    { x: 4, y: 3, viewValue: '3200x2400' },
+    { x: 16, y: 10, viewValue: '3840x2400' },
+    { x: 4, y: 3, viewValue: '4096x3072' },
+    { x: 16, y: 10, viewValue: '5120x3200' },
+    { x: 5, y: 4, viewValue: '5120x4096' },
+    { x: 25, y: 16, viewValue: '6400x4096' },
+    { x: 4, y: 3, viewValue: '6400x4800' },
+    { x: 8, y: 5, viewValue: '7680x4800' },
+    { x: 16, y: 9, viewValue: '1280x720' },
+    { x: 16, y: 9, viewValue: '1920x1080' },
+    { x: 16, y: 9, viewValue: '640x360' },
+    { x: 16, y: 9, viewValue: '960x540' },
+    { x: 16, y: 9, viewValue: '2560x1440' },
+    { x: 16, y: 9, viewValue: '2560x1440' },
+    { x: 21, y: 9, viewValue: '3440x1440' },
+    { x: 16, y: 9, viewValue: '3200x1800' },
+    { x: 16, y: 9, viewValue: '3200x1800' },
+    { x: 16, y: 9, viewValue: '3840x2160' },
+    { x: 16, y: 9, viewValue: '5120x2880' },
+    { x: 16, y: 9, viewValue: '7680x4320' },
+  ];
+
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private fb: FormBuilder) {
     iconRegistry.addSvgIcon(
       'add',
@@ -355,6 +356,8 @@ export class ComposerViewComponent implements OnInit {
       this.canvasWidth = this.canvasHeight * this.canvasProps.value.width / this.canvasProps.value.height;
     }
 
+    this.resolutionsLst = this.resolutions.filter(
+      res => res.x === this.canvasProps.value.width && res.y === this.canvasProps.value.height);
     this.resizeBackground();
     this.updateSymbolList();
   }
