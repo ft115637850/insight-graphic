993a369f5a519f2230a7116f2e7b1706070d0d98
Newton Zou <gentleman000000@hotmail.com>
2019-08-27 16:38:07 +0800

    Add landing & login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {LandingComponent} from './landing/landing.component';
 
 const routes: Routes = [
-  { path: '', loadChildren: './graphic-chart/graphic-chart.module#GraphicChartModule' }
+  { path: 'graphic-chart', loadChildren: './graphic-chart/graphic-chart.module#GraphicChartModule' },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LandingComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser'
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {GestureConfig} from '@angular/material';
+import {GestureConfig, MatButtonModule, MatInputModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LoginComponent} from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [{
     provide: HAMMER_GESTURE_CONFIG,
